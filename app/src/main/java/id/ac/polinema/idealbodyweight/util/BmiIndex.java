6b1423614956fa22193b8d2445b05d03385164b9
FauzanAr <faurinta12345@gmail.com>
2019-10-01 23:29:10 +0800

    Creating formula at BmiIndex class

--- app/src/main/java/id/ac/polinema/idealbodyweight/util/BmiIndex.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/util/BmiIndex.java
@@ -1,7 +1,37 @@
 package id.ac.polinema.idealbodyweight.util;
 
 public class BmiIndex {
-    private int height;
+    private float height;
     private int mass;
-    
+    private String result;
+
+    public BmiIndex(int mass, int height){
+        this.mass = mass;
+        this.height = height/100;
+        this.result = calculate();
+    }
+
+    private String calculate() {
+        float tmpResult;
+        tmpResult = mass  / (height*height);
+
+        if(tmpResult < 18.50){
+            return "Underweight";
+        }else if(tmpResult < 24.99){
+            return "Normal (Healthy Weight)";
+        }else if(tmpResult < 29.99){
+            return "Pre-Obese";
+        }else if (tmpResult < 34.99){
+            return "Obese Class 1";
+        }else if (tmpResult < 39.99){
+            return "Obese Class 2";
+        }else if (tmpResult > 40.00){
+            return "Obese Class 3";
+        }else
+            return "Wrong Input!!";
+    }
+
+    public String getResult(){
+        return result;
+    }
 }
